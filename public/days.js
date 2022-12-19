dd2060c0f08c8c2cb3f2a783b6c920ff12d87ae9
Allan Cueff <allan.cueff@protonmail.com>
2022-12-19 16:08:53 +0100

    hours per sport and percentage

--- public/days.js
+++ public/days.js
@@ -11,4 +11,24 @@ function getDaysArray(){
     return daysActive;
 }
 
+function getSportPercentage(){
+    const activities = JSON.parse(localStorage.getItem('activities'));
+    let yearlyDuration = 0;
+    const sportDuration = {};
+    for(const activity of activities){
+        yearlyDuration += activity.moving_time;
+        if(sportDuration[activity.sport_type] === undefined){
+            sportDuration[activity.sport_type] = activity.moving_time;
+        } else {
+            sportDuration[activity.sport_type] += activity.moving_time;
+        }
+    }
+    for(const [key, value] of Object.entries(sportDuration)){
+        console.log(key, value);
+        sportDuration[key] = value / yearlyDuration;
+    }
+    return sportDuration;    
+}
+
 console.log(getDaysArray());
+console.log(getSportPercentage())
