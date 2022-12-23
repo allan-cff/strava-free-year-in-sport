31edc21b86422af7b744ba08ece64041c5cd7d3e
Allan Cueff <allan.cueff@protonmail.com>
2022-12-23 14:48:57 +0100

    linked active days data to style

--- public/days.js
+++ public/days.js
@@ -1,11 +1,13 @@
-function getDaysArray(){
+function getDaysActive(){
     const activities = JSON.parse(localStorage.getItem('activities'));
     const daysActive = [];
+    for(let i = 0; i < 12; i++){
+        daysActive.push([]);
+    }
     for(const activity of activities){
         const actDate = new Date(activity.start_date_local);
-        const actDateString = `${actDate.getFullYear()}-${actDate.getMonth()+1}-${actDate.getDate()}`
-        if(!(daysActive.find(day => day === actDateString))){
-            daysActive.push(actDateString);
+        if(!(daysActive[actDate.getMonth()].find(date => date === actDate.getDate()))){
+            daysActive[actDate.getMonth()].push(actDate.getDate());
         }
     }
     return daysActive;
@@ -30,5 +32,33 @@ function getSportPercentage(){
     return sportDuration;    
 }
 
-console.log(getDaysArray());
-console.log(getSportPercentage())
+const sportPercentage = getSportPercentage();
+const daysActive = getDaysActive();
+
+document.querySelector(`.nombre-xl`).innerHTML = daysActive.reduce((sum, current) => sum + current.length, 0);
+
+const months = ['janvier', 'fevrier', 'mars', 'avril', 'mai', 'juin', 'juillet', 'aout', 'septembre', 'octobre', 'novembre', 'decembre'];
+const daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+for(let i = 0; i < 12; i++){
+    const monthDiv = document.querySelector(`body main .calendrier #${months[i]} .jours`);
+    for(let day = 1; day <= daysInMonth[i]; day++){
+        if(daysActive[i].find(date => date === day)){
+            monthDiv.insertAdjacentHTML('beforeend', '<div class="jour-actif"></div>');
+        } else {
+            monthDiv.insertAdjacentHTML('beforeend', '<div class="jour-inactif"></div>');
+        }
+    }
+}
+
+for(let i = 1; i < 5; i++){
+    let maxPercentage = 0;
+    let maxPercentageSport = "";
+    for(const [key, value] of Object.entries(sportPercentage)){
+        if(value > maxPercentage){
+            maxPercentage = value;
+            maxPercentageSport = key;
+        }
+    }
+    document.querySelector(`body main .top-sport #top-${i} p`).innerHTML = `${(maxPercentage*100).toFixed(0)}%<br>${maxPercentageSport}`;
+    delete sportPercentage[maxPercentageSport];
+}
