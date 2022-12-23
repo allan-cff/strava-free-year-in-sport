9c209e228a92e7a9d182e022f2c381cc225a8d93
Allan Cueff <allan.cueff@protonmail.com>
2022-12-23 21:46:27 +0100

    linked totals data to style

--- public/totals.js
+++ public/totals.js
@@ -28,6 +28,15 @@ function getDistanceByMonth(storedAs = 'activities'){
     return distanceByMonth;
 }
 
+function normalizeValue(numToNormalize){
+    if(numToNormalize > 1000){
+        numToNormalize = (numToNormalize/1000).toFixed(3);
+    } else {
+        numToNormalize = numToNormalize.toFixed(0);
+    }
+    return numToNormalize;
+}
+
 console.log(getTotals());
 console.log(getClimbByMonth());
 console.log(getDistanceByMonth());
@@ -35,3 +44,28 @@ console.log('2021 Values : ');
 console.log(getTotals('2021-activities'));
 console.log(getClimbByMonth('2021-activities'));
 console.log(getDistanceByMonth('2021-activities'));
+
+document.querySelector('.denivele .nombre-denivele').innerHTML = normalizeValue(getTotals().total.climb);
+const climbByMonth = getClimbByMonth();
+let bestMonthClimb = 0;
+let bestMonthClimbValue = 0;
+for(const [key, value] of Object.entries(climbByMonth)){
+    if(value > bestMonthClimbValue){
+        bestMonthClimbValue = value;
+        bestMonthClimb = key - 1;
+    }
+}
+const months = ['janvier', 'février', 'mars', 'avril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre'];
+document.querySelector('.denivele .record-denivele').innerHTML = `En ${months[bestMonthClimb]}, vous avez cumulé ${normalizeValue(bestMonthClimbValue)}m de d+.`;
+document.querySelector('.distance .nombre-distance').innerHTML = normalizeValue(getTotals().total.distance/1000);
+const distanceByMonth = getDistanceByMonth();
+let bestMonthDistance = 0;
+let bestMonthDistanceValue = 0;
+for(const [key, value] of Object.entries(distanceByMonth)){
+    if(value > bestMonthDistanceValue){
+        bestMonthDistanceValue = value;
+        bestMonthDistance = key - 1;
+    }
+}
+console.log(bestMonthDistanceValue)
+document.querySelector('.distance .record-distance').innerHTML = `En ${months[bestMonthDistance]}, vous avez parcouru ${normalizeValue(bestMonthDistanceValue/1000)}km - votre plus gros mois !`;
