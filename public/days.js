d67b50706e22b2e4ca54e459df8c50efb90bfefe
Allan Cueff <allan.cueff@protonmail.com>
2022-12-24 18:57:12 +0100

    added days streak and svg to best sports

--- public/days.js
+++ public/days.js
@@ -32,6 +32,15 @@ function getSportPercentage(){
     return sportDuration;    
 }
 
+function getSrc(sport){
+    if(sport === 'MountainBikeRide' || sport === 'Ride'){
+        return './images/bike-black.svg';
+    }
+    if(sport === 'Run' || sport === 'Hike'){
+        return './images/run-black.svg'
+    }
+}
+
 const sportPercentage = getSportPercentage();
 const daysActive = getDaysActive();
 
@@ -39,16 +48,25 @@ document.querySelector(`.nombre-xl`).innerHTML = daysActive.reduce((sum, current
 
 const months = ['janvier', 'fevrier', 'mars', 'avril', 'mai', 'juin', 'juillet', 'aout', 'septembre', 'octobre', 'novembre', 'decembre'];
 const daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+let streak = 0;
+let maxStreak = 0;
 for(let i = 0; i < 12; i++){
     const monthDiv = document.querySelector(`body main .calendrier #${months[i]} .jours`);
     for(let day = 1; day <= daysInMonth[i]; day++){
         if(daysActive[i].find(date => date === day)){
             monthDiv.insertAdjacentHTML('beforeend', '<div class="jour-actif"></div>');
+            streak++;
+            if(streak > maxStreak){
+                maxStreak = streak;
+            }
         } else {
             monthDiv.insertAdjacentHTML('beforeend', '<div class="jour-inactif"></div>');
+            streak = 0;
         }
     }
 }
+
+document.querySelector('.streak>p').innerHTML = maxStreak;
 
 for(let i = 1; i < 5; i++){
     let maxPercentage = 0;
@@ -60,5 +78,7 @@ for(let i = 1; i < 5; i++){
         }
     }
     document.querySelector(`body main .top-sport #top-${i} p`).innerHTML = `${(maxPercentage*100).toFixed(0)}%<br>${maxPercentageSport}`;
+    document.querySelector(`body main .top-sport #top-${i} img`).src = getSrc(maxPercentageSport);
+    document.querySelector(`body main .top-sport #top-${i} img`).alt = maxPercentageSport;
     delete sportPercentage[maxPercentageSport];
 }
