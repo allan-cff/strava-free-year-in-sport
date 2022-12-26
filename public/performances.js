ab7071bb64ee48fe1bac33ef54bf91f3b09adfd3
Allan Cueff <allan.cueff@protonmail.com>
2022-12-26 21:58:53 +0100

    added perf data

--- public/performances.js
+++ public/performances.js
@@ -12,5 +12,19 @@ function getKudosNumber(){
     return totals.total.kudos;
 }
 
+function setValue(elem, value){
+    elem.innerHTML = value;
+}
+
+function numberAnimation(selector, value, duration = 3){
+    const elem = document.querySelector(selector);
+    elem.innerHTML = 0;
+    for(let i = 0; i <= value; i++){
+        setTimeout(setValue, (i/value)*(duration*1000), elem, i);
+    }
+}
+
 console.log(getPRNumber());
 console.log(getKudosNumber());
+
+numberAnimation('.record .medaille .nombre-sm', getPRNumber());
