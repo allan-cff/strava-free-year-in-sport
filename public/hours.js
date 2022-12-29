0cd3dd013943ae81b4d1f7e3c67eabd5da7afff7
Allan Cueff <allan.cueff@protonmail.com>
2022-12-29 18:17:31 +0100

    hours bubbles size, colors, text

--- public/hours.js
+++ public/hours.js
@@ -17,9 +17,30 @@ function getHours(storedAs = 'activities'){
     return hours;    
 }
 
+function getBestMonth(hoursByMonth){
+    maxMonth = 0;
+    for(let i = 0; i < 12; i++){
+        if(hoursByMonth[i] > hoursByMonth[maxMonth]){
+            maxMonth = i;
+        }
+    }
+    return maxMonth;
+}
+
 console.log(getHoursByMonth());
 console.log(getHours());
 console.log(getHours('2021-activities'));
 
+const hoursByMonth = getHoursByMonth();
+const bestMonthId = getBestMonth(hoursByMonth);
 document.querySelector('#heures .nombre-xxl').innerHTML = getHours().toFixed(0);
 document.querySelector('#comp-21 h2:nth-child(2)').innerHTML = getHours('2021-activities').toFixed(0).toString(10) + 'H';
+const bestMonthElem = document.querySelector(`#calendrier div:nth-child(${bestMonthId + 1})`);
+bestMonthElem.classList.add("best-month");
+bestMonthElem.insertAdjacentHTML("beforeend", `<p>${hoursByMonth[bestMonthId].toFixed(0)}H</p>`)
+for(let i = 0; i < 12; i++){
+    const scale = (hoursByMonth[i] / hoursByMonth[bestMonthId]) * 1.5;
+
+    console.log(hoursByMonth[i].toFixed(0), hoursByMonth[bestMonthId].toFixed(0), scale.toFixed(3));
+    document.querySelector(`#calendrier div:nth-child(${i + 1})`).style.transform = `scale(${scale})`;
+}
