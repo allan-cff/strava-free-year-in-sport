cd6c86aab2a60146eb081d2aa1ac7fc1ffc79e2a
Allan Cueff <allan.cueff@protonmail.com>
2022-12-29 18:25:42 +0100

    default values set to 0 & "no data"

--- public/pictures.js
+++ public/pictures.js
@@ -5,6 +5,7 @@ let i = 0;
 let ids = ['premier', 'deuxieme', 'troisieme', 'quatrieme'];
 for(const activity of bestPicturesActivitiesObjects){
     document.querySelector(`.meilleures-photos #${ids[i]} img`).src = activity.photos.primary.urls[600];
+    document.querySelector(`.meilleures-photos #${ids[i]} img`).alt = activity.name;
     document.querySelector(`.meilleures-photos #${ids[i]} .pictures-info .pictures-nom p`).innerHTML = activity.name;
     document.querySelector(`.meilleures-photos #${ids[i]} .pictures-info .pictures-horaires p:nth-child(1)`).innerHTML = new Date(activity.start_date_local).toLocaleDateString(undefined, { month: 'long', day: 'numeric' });
     document.querySelector(`.meilleures-photos #${ids[i]} .pictures-info .pictures-horaires p:nth-child(2)`).innerHTML = new Date(activity.start_date_local).toLocaleTimeString();
